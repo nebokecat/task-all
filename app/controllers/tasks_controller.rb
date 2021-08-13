6d05e833413230b32ad15374727c5012928b713c
nebokecat <neboke.cat@gmail.com>
2021-08-13 15:52:39 +0900

    tasksコントローラにcreate,updateアクションの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task,only: [:show]
+  before_action :set_task,only: [:show,:update]
+
   def index
     @tasks = Task.all
     render json: {tasks: @tasks}
@@ -9,8 +10,30 @@ class TasksController < ApplicationController
     render json: {task: @task}
   end
 
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      render json: {task: @task}
+    else
+      render json: {errors: @task.errors}
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      render json: {task: @task}
+    else
+      render json: {errors: @task.errors}
+    end
+  end
+
+  private
+
   def set_task
     @task = Task.find(params[:id])
   end
 
+  def task_params
+    params.require(:task).permit(:name,:description)
+  end
 end

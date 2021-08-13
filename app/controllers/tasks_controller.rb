291f4a69ecb174ca2b40ea8df7a6a6f7a06e9d39
nebokecat <neboke.cat@gmail.com>
2021-08-13 13:05:03 +0900

    tasksコントローラーの追加 & index showの追加

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,16 @@
+class TasksController < ApplicationController
+  before_action :set_task,only: [:show]
+  def index
+    @tasks = Task.all
+    render json: {tasks: @tasks}
+  end
+
+  def show
+    render json: {task: @task}
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+end

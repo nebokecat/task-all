4c42e830104d29770ef156ba50377ac28fea7aba
nebokecat <neboke.cat@gmail.com>
2021-08-13 18:47:11 +0900

    tasksコントローラにdestroyアクションの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,30 +1,34 @@
 class TasksController < ApplicationController
-  before_action :set_task,only: [:show,:update]
+  before_action :set_task, only: %i[show update destroy]
 
   def index
     @tasks = Task.all
-    render json: {tasks: @tasks}
+    render json: { tasks: @tasks }
   end
 
   def show
-    render json: {task: @task}
+    render json: { task: @task }
   end
 
   def create
     @task = Task.new(task_params)
     if @task.save
-      render json: {task: @task}
+      render json: { task: @task }
     else
-      render json: {errors: @task.errors}
+      render json: { errors: @task.errors }
     end
   end
 
   def update
     if @task.update(task_params)
-      render json: {task: @task}
+      render json: { task: @task }
     else
-      render json: {errors: @task.errors}
+      render json: { errors: @task.errors }
     end
+  end
+
+  def destroy
+    @task.destroy
   end
 
   private
@@ -34,6 +38,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:name,:description)
+    params.require(:task).permit(:name, :description)
   end
 end

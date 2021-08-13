a2ecd34b31858df7d6f1c367920202caff6c066a
nebokecat <neboke.cat@gmail.com>
2021-08-13 16:14:15 +0900

    tasksコントローラのrubocopに合わせた修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task,only: [:show,:update]
+  before_action :set_task,only: [:show]
 
   def index
     @tasks = Task.all
@@ -10,30 +10,7 @@ class TasksController < ApplicationController
     render json: {task: @task}
   end
 
-  def create
-    @task = Task.new(task_params)
-    if @task.save
-      render json: {task: @task}
-    else
-      render json: {errors: @task.errors}
-    end
-  end
-
-  def update
-    if @task.update(task_params)
-      render json: {task: @task}
-    else
-      render json: {errors: @task.errors}
-    end
-  end
-
-  private
-
   def set_task
     @task = Task.find(params[:id])
-  end
-
-  def task_params
-    params.require(:task).permit(:name,:description)
   end
 end

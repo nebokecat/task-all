d84e0f31b74337dd9e5e0614898e569ec0100cb7
nebokecat <neboke.cat@gmail.com>
2021-09-17 18:59:08 +0900

    task_spec修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  before do
-    @task = build(:task)
-  end
+
+  let(:task){ build(:task) }
 
   describe 'バリデーション' do
     context '正常形' do
       it 'タイトルと説明文がある場合' do
-        expect(@task).to be_valid
+        expect(task).to be_valid
       end
     end
 
     context '異常形' do
       it 'タイトルが空の場合' do
-        @task.name = ''
-        expect(@task).to be_invalid
+        task.name = nil
+        expect(task).to be_invalid
       end
+
       it '説明文が空の場合' do
-        @task.description = ''
-        expect(@task).to be_invalid
+        task.description = nil
+        expect(task).to be_invalid
       end
     end
   end

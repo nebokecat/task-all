9d2357d37ed20827ef6f7cfa506c4a1eac8aad1d
nebokecat <neboke.cat@gmail.com>
2021-09-30 18:48:05 +0900

    不要なコメント削除

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,14 +3,12 @@ class Task < ApplicationRecord
   validates :description, presence: true
   enum status: { '未着手': 0, '着手中': 1, '完了': 2 }
 
-  # 　動作確認観点：curl 叩いて部分一致できているか
   scope :with_title, lambda { |title|
     where('name LIKE ?', "%#{title}%")
   }
 
   scope :with_status, ->(status) { where(status: status) }
 
-  # 　動作確認観点：curl 叩いて以下4パターンのロジックが動作しているか
   def self.search(title, status)
     if title.nil? && status.nil?
       # 検索なし

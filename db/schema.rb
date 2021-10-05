7e8dce0f7797962509b6061a26fdaee5d169396c
nebokecat <neboke.cat@gmail.com>
2021-10-05 12:30:49 +0900

    add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_055403) do
+ActiveRecord::Schema.define(version: 2021_10_01_023606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,18 @@ ActiveRecord::Schema.define(version: 2021_09_30_055403) do
     t.datetime "updated_at", null: false
     t.datetime "finished_at"
     t.integer "status", default: 0
+    t.bigint "user_id"
     t.index ["name"], name: "index_tasks_on_name"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "tasks", "users"
 end
